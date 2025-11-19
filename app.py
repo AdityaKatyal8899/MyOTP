9d83d30ff9057673d8adb24a3963760eec427652
Aditya <adityakatyal12345@gmail.com>
2025-11-19 23:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Exotel IVR Server Running"
+    return "Hi"
 
 @app.route("/exotel.xml")
 def exotel_xml():
@@ -16,4 +16,4 @@ def exotel_xml():
     return Response(xml, mimetype="text/xml")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000) 
